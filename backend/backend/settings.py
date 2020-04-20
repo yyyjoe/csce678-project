fa5705e35f865afa21653bfd75a53f77b1195eda
b01901143 <b01901143@ntu.edu.tw>
2020-04-20 16:02:43 +0000

    add django-cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django-cors-headers setting
+CORS_ORIGIN_ALLOW_ALL = True
